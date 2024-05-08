b139a57e370437dacbb1c636f5d1d6e3ccb53644
avgmensch <124395199+avgmensch@users.noreply.github.com>
2024-05-08 13:33:23 +0200

    Explain code in tkinter_resize_demo.py

--- tkinter_resize_demo.py
+++ tkinter_resize_demo.py
@@ -3,40 +3,70 @@ import tkinter.ttk as ttk
 
 
 class TkinterResizeDemoApp:
-    def __init__(
-            self,
-            window: tk.Tk | None = None,
-            grid_size: tuple[int, int] = (10, 10)
-    ) -> None:
+    """App that creates a window with elements, that resize with the window."""
 
-        # Setup window
+    def __init__(
+        self,
+        window: tk.Tk | None = None,
+        grid_size: tuple[int, int] = (10, 10)
+    ) -> None:
+        """
+        Create a new instance of `TkinterResizeDemoApp`.
+
+        :param window: If you want, you can provide a custom `Tk` here. Be \
+        aware that if you do so, no settings will be applied to `tk` by \
+        `__init__()` (this may cause the window to not resize as intended).
+
+        :param grid_size: Defines how many elements will be put on the \
+        screen. Defaults to `(10, 10)`. Enter by pattern `(x, y)`.
+        """
+
+        # SETUP WINDOW
+        # Create a window is None was provided
         user_provided_window = window is not None
         self.tk = window if user_provided_window else tk.Tk()
+        # Apply required settings for resizing to the window
+        # TODO: Add option to let __init__ configure
+        #       the Tk, even if window is None
         if not user_provided_window:
             self.tk.title("Tkinter Resize Demo")
-            self.tk.columnconfigure(0, weight=1)
-            self.tk.rowconfigure(0, weight=1)
+            # The next two lines get explained later in the code
+            self.tk.columnconfigure(0, weight=1)  # <- IMPORTANT
+            self.tk.rowconfigure(0, weight=1)  # <- IMPORTANT
 
-        # Root level frame
+        # ROOT LEVEL FRAME
+        # The root level frame should be the only child of the window
         self.button_grid = ttk.Frame(self.tk, padding=2)
+        # sticky="nesw" tells Tk that the Frame should stick to all borders
+        # n = NORTH ; e = EAST ; s = SOUTH ; w = WEST
         self.button_grid.grid(column=0, row=0, sticky="nesw")
 
-        # Create buttons
+        # CREATE AND DISPLAY BUTTONS
+        # Store the size of the grid
         self.grid_size = grid_size
+        # Store the elements in the grid
         self.buttons: list[list[ttk.Button]] = []
+        # Create the elements in the grid
+        # First for loop is for y, because the list if row-major
         for y in range(self.grid_size[1]):
+            # Append row and create elements in columns
             self.buttons.append([])
             for x in range(self.grid_size[0]):
-                self.button_grid.columnconfigure(x, weight=1)
-                self.button_grid.rowconfigure(y, weight=1)
+                # Set the weight value of the grid elements to 1, so they can
+                # expand. If they were 0 nothing would happen when resizing.
+                self.button_grid.columnconfigure(x, weight=1)  # <- IMPORTANT
+                self.button_grid.rowconfigure(y, weight=1)  # <- IMPORTANT
+                # Create a new button
                 self.buttons[y].append(ttk.Button(
                     master=self.button_grid,
                     text=f"x/col={x}\ny/row={y}",
                     command=lambda: print(f"\nx/col={x}\ny/row={y}"),
                     padding=2))
+                # Display the button on the screen. Make sure to set sticky.
                 self.buttons[y][x].grid(column=x, row=y, sticky="nesw")
 
     def mainloop(self, n: int = 0) -> None:
+        """Starts the application."""
         self.tk.mainloop(n)
 
 
